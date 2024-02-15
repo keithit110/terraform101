a142157e5f783533d3a69e3d6bde983d29da23d1
KeithRivera <keith5132007@hotmail.com>
2024-02-15 15:03:47 +0000

    creating a delay

---
+++ delay-sleep/main.tf
@@ -0,0 +1,38 @@
+/* Alta3 Research - rzfeeser@alta3.com
+   An example of creating an intentional delay with Terraform. In most cases,
+   doing something like this should be considered a "work-around". */
+
+terraform {
+  required_providers {
+    null = {
+      source = "hashicorp/null"
+      version = "3.1.1"
+    }
+  }
+}
+
+// if no customizations are present
+// the provider block is optional
+provider "null" {
+  # Configuration options
+}
+
+resource "null_resource" "previous" {
+  provisioner "local-exec" {
+    command = "echo 'keith' > file2.txt"
+  }
+}
+
+
+
+
+resource "null_resource" "next" {
+  depends_on = [time_sleep.wait_30_seconds]
+}
+
+
+resource "time_sleep" "wait_30_seconds" {
+  depends_on = [null_resource.previous]
+  create_duration = "19s"   // pause this long when a creation occurs
+  destroy_duration = "9s"  // pause this long when a destroy occurs
+}
